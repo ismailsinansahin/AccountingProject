22f906e5baebd3e23a09d681646b9bd05dde395a
bahatas <baha@cybetekschool.com>
2022-02-10 11:09:21 +0000

    Product Controller Envelope and response

--- service-product/src/main/java/com/cydeo/serviceproduct/controller/ProductController.java
+++ service-product/src/main/java/com/cydeo/serviceproduct/controller/ProductController.java
@@ -1,9 +1,22 @@
 package com.cydeo.serviceproduct.controller;
 
+import com.cydeo.serviceproduct.dto.ResultEnvelope;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/product")
 public class ProductController {
 
+    @GetMapping("/test")
+    public ResultEnvelope<ResponseEntity> getProctList(){
+
+        String initialResponse = "This is test response from Product Service";
+
+        return ResultEnvelope.ok(initialResponse);
+    }
 
 }
--- service-product/src/main/java/com/cydeo/serviceproduct/repository/ProductRepository.java
+++ service-product/src/main/java/com/cydeo/serviceproduct/repository/ProductRepository.java
@@ -7,6 +7,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    Product findCompanyByCompanyName(String productId);
 
 }
