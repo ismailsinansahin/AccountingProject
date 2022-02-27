9dc9b498ea9c34ded1f6011d39a22eb9d21d4ea4
BAHATTIN TASTAN <59250233+bahatas@users.noreply.github.com>
2022-02-27 16:09:10 +0000

    payment service creates config file update

---
+++ service-payment/src/main/java/com/cydeo/servicepayment/ServicePaymentApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo.servicepayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicePaymentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServicePaymentApplication.class, args);
+    }
+
+}
