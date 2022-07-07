d8790c70333b1fd99a50928ae770c3944ecf41ef
otto <otto.o@cybertekschool.com>
2022-07-07 10:04:09 +0300

    controller implementation..

--- service-report/src/main/java/com/cydeo/servicereport/implementation/ReportServiceImplementation.java
+++ service-report/src/main/java/com/cydeo/servicereport/implementation/ReportServiceImplementation.java
@@ -4,10 +4,12 @@ import com.cydeo.servicecommon.contract.ReportDto;
 import com.cydeo.servicereport.mapper.MapperUtil;
 import com.cydeo.servicereport.repository.ReportRepository;
 import com.cydeo.servicereport.service.ReportService;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Service
 public class ReportServiceImplementation implements ReportService {
 
     private ReportRepository reportRepository;
--- service-report/src/main/java/com/cydeo/servicereport/service/ReportService.java
+++ service-report/src/main/java/com/cydeo/servicereport/service/ReportService.java
@@ -5,7 +5,6 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
 public interface ReportService {
     List<ReportDto> findAllReports();
 }
