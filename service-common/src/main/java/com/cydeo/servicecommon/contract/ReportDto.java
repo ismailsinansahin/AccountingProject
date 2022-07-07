9ec629181f70d90043da3da6128819180902c2b3
otto <otto.o@cybertekschool.com>
2022-07-07 09:22:55 +0300

    dto done..

---
+++ service-common/src/main/java/com/cydeo/servicecommon/contract/ReportDto.java
@@ -0,0 +1,31 @@
+package com.cydeo.servicecommon.contract;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonIgnoreProperties(value = {"hibernateLazyInitializer"},ignoreUnknown = true)
+public class ReportDto {
+
+    private Long id;
+
+    private LocalDate invoiceDate;
+
+    private int price;
+
+    private int quantity;
+
+    private int remainingQuantity;
+
+    @JsonProperty("product_id")
+    private Long product;
+}
---
+++ service-report/src/main/java/com/cydeo/servicereport/service/ReportService.java
@@ -0,0 +1,4 @@
+package com.cydeo.servicereport.service;
+
+public interface ReportService {
+}
